12026f7ca7fa0dde74f2c204e99df1e1ce457d6f
Dana <Dana.ackerman0914@gmail.com>
2024-07-01 17:31:09 +0500

    Refactored code in main method

--- src/main/java/filereader/Main.java
+++ src/main/java/filereader/Main.java
@@ -1,46 +1,28 @@
 package filereader;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.BusTicket;
 import validator.Validator;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+
 
 public class Main {
-    static BufferedReader reader;
-    static {
-        try {
-            reader = new BufferedReader(new FileReader("src/main/java/file.txt"));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/java/file.txt"));
+        getInputFromFile(reader);
     }
-
-    public static String getInputFromFile() {
-        try {
-            return reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Error reading file " + e.getMessage());
-        }
-        return null;
-
-    }
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void getInputFromFile(BufferedReader reader) throws IOException {
         BusTicket ticket;
-        int x = 0;
-        do {
-            String input = getInputFromFile();
-            ticket = new ObjectMapper().readValue(input, BusTicket.class);
-            System.out.println(new Validator().validateBusTicket(ticket));
+        Validator validator = new Validator();
+        ObjectMapper mapper = new ObjectMapper();
+        String input;
+        while ((input = reader.readLine()) != null) {
+            ticket = mapper.readValue(input, BusTicket.class);
             System.out.println(ticket);
-            x++;
-        } while (x < 17);
-
-        new Validator().getPopularViolation();
+            System.out.println(validator.validateBusTicket(ticket));
+        }
+        validator.getPopularViolation();
     }
-
 }
